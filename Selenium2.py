32bb1e69274735b3b2a2a4aff118718c170d6b3a
Łukasz Saletnik <luksaletnik@gmail.com>
2023-11-06 18:08:30 +0100

    Added file

--- Selenium2.py
+++ Selenium2.py
@@ -6,3 +6,28 @@ from selenium.webdriver.support import expected_conditions as excon
 from Selenium1 import make_screenshot
 
 
+def wait_for_id(id_element):
+    timeout = 5
+    timeout_message = f'Element with id {id_element} did not appear within {timeout} seconds'
+    localizator = (By.ID, id_element)
+    found = excon.visibility_of_element_located(localizator)  # sprawdzamy, czy element widoczny
+    waiter = WebDriverWait(driver, timeout)  # jak dlugo czekac i gdzie
+    return waiter.until(found, timeout_message)  # zwrotka
+
+
+driver = webdriver.Chrome()
+driver.get('https://www.saucedemo.com')
+
+# login_button = driver.find_element(By.ID, 'login-button')
+
+try:
+    login_button = wait_for_id('login-button')
+except:
+    print('Not found')
+    raise
+else:
+    print('Found')
+finally:
+    print('End of test')
+    make_screenshot(driver)
+    driver.quit()
